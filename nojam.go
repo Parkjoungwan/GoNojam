2b14f2abee0777a32836ab6c01196cf0de422a56
joungwanpark <whddhs6645@gmail.com>
2020-04-21 17:58:46 +0900

    1002

--- nojam.go
+++ nojam.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func nojam11050() {
 	var a int
@@ -73,8 +76,37 @@ func nojam10988() {
 	} else {
 		fmt.Println(0)
 	}
-
 }
+func nojam1002() {
+	var x1, x2, y1, y2, r1, r2 float64
+	var count int
+	fmt.Scan(&count)
+	for i := 0; i < count; i++ {
+		fmt.Scan(&x1, &y1, &r1, &x2, &y2, &r2)
+		bewTwo := (x1-x2)*(x1-x2) + (y2-y1)*(y2-y1)
+		bewTwo = math.Sqrt(bewTwo)
+		if x1 == x2 && y1 == y2 {
+			if r1 == r2 {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(0)
+			}
+		} else if bewTwo == r1+r2 {
+			fmt.Println(1)
+		} else if bewTwo < r1+r2 {
+			if math.Abs(r2-r1) > bewTwo {
+				fmt.Println(0)
+			} else if math.Abs(r2-r1) == bewTwo {
+				fmt.Println(1)
+			} else {
+				fmt.Println(2)
+			}
+		} else if bewTwo > r1+r2 {
+			fmt.Println(0)
+		}
+	}
+}
+
 func main() {
-	nojam10988()
+	nojam1002()
 }
