b0dda4a9f825be1550636c3ba4b86f5747743c85
parkjoungwan_home <whddhs6645@gmail.com>
2020-04-27 03:04:08 +0900

    1041

--- nojam.go
+++ nojam.go
@@ -103,6 +103,65 @@ func nojam1059() {
 	fmt.Println(down, Unlukey, up)
 	fmt.Println((Unlukey-down)*(up-Unlukey) - 1)
 }
+func nojam1041() {
+	var N int
+	fmt.Scan(&N)
+	var A, B, C, D, E, F int
+	fmt.Scan(&A, &B, &C, &D, &E, &F)
+	Num := make([]int, 6)
+	Num[0] = A
+	Num[1] = B
+	Num[2] = C
+	Num[3] = D
+	Num[4] = E
+	Num[5] = F
+	sort.Sort(sort.IntSlice(Num))
+	for i := 1; i < 5; i++ {
+		Num[0] += Num[i]
+	}
+	if A > F {
+		A = F
+	}
+	if B > E {
+		B = E
+	}
+	if C > D {
+		C = D
+	}
+	var for3, for2, for1 int
+	for3 = A + B + C
+	if A >= B && A >= C {
+		for2 = B + C
+		if B > C {
+			for1 = C
+		} else {
+			for1 = B
+		}
+	} else if B >= C && B >= A {
+		for2 = A + C
+		if A > C {
+			for1 = C
+		} else {
+			for1 = A
+		}
+	} else {
+		for2 = A + B
+		if A > B {
+			for1 = B
+		} else {
+			for1 = A
+		}
+	}
+	for3 = 4 * for3
+	for2 = (8*N - 12) * for2
+	for1 = (5*N*N - 16*N + 12) * for1
+	ans := for3 + for2 + for1
+	if N == 1 {
+		fmt.Println(Num[0])
+	} else {
+		fmt.Println(ans)
+	}
+}
 func main() {
-	nojam1059()
+	nojam1041()
 }
