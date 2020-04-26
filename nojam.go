596c28844a3f7bc689bd0cba2e1f99ceeb14ca9f
joungwanpark <whddhs6645@gmail.com>
2020-04-26 21:14:23 +0900

    9633

--- nojam.go
+++ nojam.go
@@ -5,180 +5,46 @@ import (
 	"math"
 )
 
-func nojam11050() {
-	var a int
-	var b int
+var ans int
 
-	fmt.Scan(&a)
-	fmt.Scan(&b)
-	var FacA int = 1
-	var FacB int = 1
-	var FacC int = 1
-	for i := 1; i <= a; i++ {
-		FacA *= i
+func nojam9663() {
+	var N int
+	fmt.Scan(&N)
+	for i := 1; i <= N; i++ {
+		var Col []int = make([]int, 15)
+		Col[1] = i
+		nojam9633dfs(1, N, Col)
 	}
-	for j := 1; j <= b; j++ {
-		FacB *= j
-	}
-	for k := 1; k <= (a - b); k++ {
-		FacC *= k
-	}
-	var resultA int
-	resultA = FacA / (FacB * FacC)
-	fmt.Println(resultA)
+	fmt.Println(ans)
 }
 
-func nojam2743() {
-	var a string
-	fmt.Scan(&a)
-	fmt.Println(len(a))
-	for _, s := range a {
-		fmt.Println(string(s)) // string은 C랑 다르게 마지막에 '\0'가 없다.
-	}
-}
-
-func nojam4999() {
-	var me string
-	var doc string
-	fmt.Scan(&me)
-	fmt.Scan(&doc)
-	var mecount int
-	var doccount int
-	for _, i := range me {
-		if string(i) == "a" {
-			mecount++
-		}
-	}
-	for _, j := range doc {
-		if string(j) == "a" {
-			doccount++
-		}
-	}
-
-	if mecount >= doccount {
-		fmt.Println("go")
-		return
-	}
-	fmt.Println("no")
-}
-
-func nojam10988() {
-	var palen string
-	fmt.Scan(&palen)
-	var count int
-	for i := 0; i < len(palen)/2; i++ {
-		if palen[i] == palen[len(palen)-i-1] {
-			count++
-		}
-	}
-	if count == len(palen)/2 {
-		fmt.Println(1)
+func nojam9633dfs(Row int, N int, Col []int) {
+	if Row == N {
+		ans++
 	} else {
-		fmt.Println(0)
-	}
-}
-func nojam1002() {
-	var x1, x2, y1, y2, r1, r2 float64
-	var count int
-	fmt.Scan(&count)
-	for i := 0; i < count; i++ {
-		fmt.Scan(&x1, &y1, &r1, &x2, &y2, &r2)
-		bewTwo := (x1-x2)*(x1-x2) + (y2-y1)*(y2-y1)
-		bewTwo = math.Sqrt(bewTwo)
-		if x1 == x2 && y1 == y2 {
-			if r1 == r2 {
-				fmt.Println(-1)
+		for i := 1; i <= N; i++ {
+			Col[Row+1] = i
+			if nojam9633Possible(Row+1, Col) {
+				nojam9633dfs(Row+1, N, Col)
 			} else {
-				fmt.Println(0)
+				Col[Row+1] = 0
 			}
-		} else if bewTwo == r1+r2 {
-			fmt.Println(1)
-		} else if bewTwo < r1+r2 {
-			if math.Abs(r2-r1) > bewTwo {
-				fmt.Println(0)
-			} else if math.Abs(r2-r1) == bewTwo {
-				fmt.Println(1)
-			} else {
-				fmt.Println(2)
-			}
-		} else if bewTwo > r1+r2 {
-			fmt.Println(0)
 		}
 	}
 }
 
-func nojam2941() {
-	var Cro string
-	var count int
-	var count2 int
-	var dzcount int
-	fmt.Scan(&Cro)
-	for i := 0; i < len(Cro); i++ {
-		if string(Cro[i]) == "c" {
-			if i == len(Cro)-1 {
-				break
-			}
-			if string(Cro[i+1]) == "=" || string(Cro[i+1]) == "-" {
-				count++
-				count2 += 2
-			}
-		} else if string(Cro[i]) == "d" {
-			if i == len(Cro)-1 {
-				break
-			}
-			if i == len(Cro)-1 {
-				break
-			}
-			if string(Cro[i+1]) == "-" {
-				count++
-				count2 += 2
-			} else if string(Cro[i+1]) == "z" {
-				if i+1 == len(Cro)-1 {
-					break
-				}
-				if string(Cro[i+2]) == "=" {
-					count++
-					count2++
-					dzcount++
-				}
-			}
-		} else if string(Cro[i]) == "l" {
-			if i == len(Cro)-1 {
-				break
-			}
-			if string(Cro[i+1]) == "j" {
-				count++
-				count2 += 2
-			}
-		} else if string(Cro[i]) == "n" {
-			if i == len(Cro)-1 {
-				break
-			}
-			if string(Cro[i+1]) == "j" {
-				count++
-				count2 += 2
-			}
-		} else if string(Cro[i]) == "s" {
-			if i == len(Cro)-1 {
-				break
-			}
-			if string(Cro[i+1]) == "=" {
-				count++
-				count2 += 2
-			}
-		} else if string(Cro[i]) == "z" {
-			if i == len(Cro)-1 {
-				break
-			}
-			if string(Cro[i+1]) == "=" {
-				count++
-				count2 += 2
-			}
+func nojam9633Possible(C int, Col []int) bool {
+	for i := 1; i < C; i++ {
+		if Col[i] == Col[C] {
+			return false
+		}
+		if math.Abs(float64(Col[i]-Col[C])) == math.Abs(float64(i-C)) {
+			return false
 		}
 	}
-	fmt.Println((len(Cro) - count2) + (count - dzcount))
+	return true
 }
 
 func main() {
-	nojam2941()
+	nojam9663()
 }
