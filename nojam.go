d4ef9b1c5d1abdd44ceaa698fcf94518f087eedc
parkjoungwan_home <whddhs6645@gmail.com>
2020-04-29 05:11:02 +0900

    1929

--- nojam.go
+++ nojam.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"sort"
+	"strconv"
 )
 
 func nojam2309() {
@@ -141,7 +143,67 @@ func nojam10610() {
 		}
 	}
 }
+func nojam1929() {
+	wr := bufio.NewWriter(os.Stdout)
+	defer wr.Flush()
+	var N, M int
+	fmt.Scan(&N, &M)
+	var PN [1000001]int
+	PN[1] = 1
+	for i := 2; i <= M; i++ {
+		if PN[i] == 0 {
+			for j := i * i; j <= M; j += i {
+				PN[j] = 1
+			}
+		}
+	}
+	for i := N; i <= M; i++ {
+		if PN[i] != 1 {
+			fmt.Fprintln(wr, i) //이거 출력이 안될지도?
+		}
+	}
+}
+
+func nojam9020() {
+	wr := bufio.NewWriter(os.Stdout)
+	sc := bufio.NewScanner(os.Stdin)
+	defer wr.Flush()
+	var T int
+	fmt.Scan(&T)
+	var PN [10001]int
+	var Gold int
+	var gold1, gold2 int
+	for i := 0; i < T; i++ {
+		check := 10001.0
+		if sc.Scan() {
+			Gold, _ = strconv.Atoi(sc.Text())
+		}
+		for i := 2; i <= Gold; i++ {
+			if PN[i] == 0 {
+				for j := i * i; j <= Gold; j += i {
+					PN[j] = 1
+				}
+			}
+		}
+		for j := 2; j < Gold; j++ {
+			for k := 2; k < Gold; k++ {
+				if PN[j] != 1 && PN[k] != 1 && j+k == Gold {
+					if check > math.Abs(float64(k-j)) {
+						gold1 = j
+						gold2 = k
+						check = math.Abs(float64(k - j))
+					}
+				}
+			}
+		}
+		if gold1 > gold2 {
+			gold1, gold2 = gold2, gold1
+		}
+		fmt.Fprintf(wr, "%d %d\n", gold1, gold2)
+		check = 10001
+	}
+}
 
 func main() {
-	nojam10610()
+	nojam9020()
 }
